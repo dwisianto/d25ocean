a78cb4485bf40a54c56b6ba0ff10dcf40b1ffea9
dwisianto <dwyk@dwyk16.lan>
2024-11-27 12:25:12 -0500

    do2410

---
+++ do/do2410/do4a_access_key.sh
@@ -0,0 +1,37 @@
+#
+# [] reading the environment file
+#
+source do24.env
+
+
+#
+#
+#
+log(){ local now=$(date +'%y-%m-%d %H:%M:%S') && echo "[$now] $1" ; }
+do4_(){
+    log "DO24_HOME   ${DO24_HOME}"
+    log "do_user     ${do_user}"
+    log "do_pass     ${do_pass}"
+    log "do_address  ${do_address}"
+}
+
+
+# 
+# [] ssh-keygen
+# cd ~/.ssh
+# ssh-keygen -f id_rsa_do24.pub -C xxx@yyy.zzz
+# chmod 600 ~/.ssh/xx_xxx
+#
+HOME_SSH=${HOME}/.ssh
+do4_ssh_cfg=$HOME_SSH/config
+do4_ssh_key=$HOME_SSH/id_rsa_do24
+do4_ssh_key_pub=$HOME_SSH/id_rsa_do24.pub
+
+
+#
+# https://docs.digitalocean.com/products/droplets/how-to/add-ssh-keys/
+#
+do4_connection="${do_user}@${do_address}"  
+do4_ssh_copy_id="ssh-copy-id ${do4_connection}"
+do4_ssh_copy_(){ echo ${do4_ssh_copy_id} ; } 
+do4_ssh_copy__(){ $(do4_ssh_copy_) ; } 
---
+++ do/do2410/do4b_bash_and_ssh.sh
@@ -0,0 +1,66 @@
+
+#
+# []ssh into the server using the bash shell
+#
+source do24.env
+source ${DO24_HOME}/do2410/do4a_access_key.sh
+
+#
+# []do4q4
+#
+
+do4_ssh_x="ssh -i ${do4_ssh_key} ${do4_connection}"
+
+do4_ssh(){ fname=${FUNCNAME[0]} && log "${fname} : ${fname}_, ${fname}__ " ; } 
+do4_ssh_(){ echo ${do4_ssh_x} ; } 
+do4_ssh__(){ $(do4_ssh_) ; }
+
+#
+#
+#
+do4_scp_in__zip_folder="${DO24_HOME}/do24_setup"
+do4_scp_in__zip_file="${do4_scp_in__zip_folder}_$(date +%F).zip"
+do4_scp_in__zip_cmd="zip -r ${do4_scp_in__zip_file} ${do4_scp_in__zip_folder}"
+do4_scp_in__zip(){ 
+    echo """ 
+        folder : ${do4_scp_in__zip_folder}
+        file: ${do4_scp_in__zip_file}
+    """ 
+}
+do4_scp_in__zip_(){ echo ${do4_scp_in__zip_cmd} ; }
+do4_scp_in__zip__(){ $(do4_scp_in__zip_) ; }
+
+
+#
+#
+#
+do4_scp_in_src=${do4_scp_in__zip_file}
+do4_scp_in_tgt='~/'
+do4_scp_in_cmd="scp -i ${do4_ssh_key} ${do4_scp_in_src} ${do4_connection}:${do4_scp_in_tgt}"
+do4_scp_in(){ 
+    echo """
+    Secure Copy using Key: ${do4_ssh_key}
+        From       : ${do4_scp_in_src}
+        To         : ${do4_connection}:${do4_scp_in_tgt}
+    """
+}
+do4_scp_in_(){ echo ${do4_scp_in_cmd} ; } 
+do4_scp_in__(){ $(do4_scp_in_)  ; }
+
+
+#
+#
+#
+do4_scp_out_zip_file=''
+do4_scp_out_src=${DO24_SERVER}/${do4_scp_out_zip_file}
+do4_scp_out_tgt=${DO24_HOME}
+do4_scp_out_cmd="scp -i ${do4_ssh_key} ${do4_connection}:${do4_scp_out_src} ${do4_scp_out_tgt}"
+do4_scp_out(){ 
+    echo """
+    Secure Copy using Key:${do4_ssh_key} 
+        From       : ${do4_connection}:${do4_scp_out_src}
+        To         : ${do4_scp_out_tgt}
+    """
+}
+do4_scp_out_(){ echo ${do4_scp_out_cmd} }
+do4_scp_out__(){ $(do4_scp_out_) }
